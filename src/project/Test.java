8991c7839a3f80504fb2ee8ed1828bb46d12b5fa
Praveen Viswanathan <141003188+PraveenAIeng@users.noreply.github.com>
2025-07-17 18:36:43 +0530

    Update Test.java

--- src/project/Test.java
+++ src/project/Test.java
@@ -6,8 +6,9 @@ import java.util.*;
 public class Test {
 	public static void main(String args[] ) {
 		ArrayList<String> array = new ArrayList<>();
-		Customer c4 = new Customer("Praveen", 12, 21, array);
-		System.out.println(c4.age);
+		Customer c4 = new Customer("Praveenzohocrm", 12, 21, array);
+		System.out.println("Praveenzohocrm");
+		System.out.println(c4.age +"testing");
 	}
 	
 }
